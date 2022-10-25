fc369833487e6d2028b67d519692ca7c2b178be3
OkazakiRui <okazaki200226@gmail.com>
2022-10-25 22:29:37 +0900

    add: mantine ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: "shiki",
   },
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 });
